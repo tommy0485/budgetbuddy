ed9d38b31d502ae8222a629b347f56c6199c850e
Aaron Hove <aaron.hove@umontana.edu>
2023-12-10 22:24:02 -0700

    first commit

---
+++ app/src/main/java/com/example/budgetBuddy/IntroFragment.kt
@@ -0,0 +1,44 @@
+package com.example.budgetBuddy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.budgetBuddy.databinding.FragmentIntroBinding
+//import com.example.budgetBuddy.databinding.FragmentIntroBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class IntroFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentIntroBinding? = null
+    private val binding get() = _binding!!
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentIntroBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment3_to_itemListFragment)
+        }
+        return root
+    }
+}
